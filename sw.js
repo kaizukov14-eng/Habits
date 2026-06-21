1e5b0e0ff3820e6278a5e2bfa95d6d1f435831ca
kaizukov14-eng <kaizukov14@gmail.com>
2026-06-21 19:09:35 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'habits-cache-v1';
+const ASSETS = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(key){ return key !== CACHE_NAME; })
+            .map(function(key){ return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      return cached || fetch(event.request).then(function(response){
+        return caches.open(CACHE_NAME).then(function(cache){
+          if(event.request.method === 'GET' && response.status === 200){
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      }).catch(function(){
+        return cached;
+      });
+    })
+  );
+});
